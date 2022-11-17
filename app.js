09c842e45e481c9306fef4cf82c3884ba68cd2ee
vnguyenlong <nvlong567@gmail.com>
2022-11-17 17:13:30 +0700

    [++] auth, login

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 require('dotenv/config');
 
 app.use(cors());
@@ -12,6 +14,8 @@ app.options('*', cors());
 //Middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
+app.use(authJwt());
+app.use(errorHandler);
 
 
 //Routers
